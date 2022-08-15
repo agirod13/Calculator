b59f9b787e5a5d47a12b6f282bd3a815894bbe1a
agirod13 <agirod13@gmail.com>
2022-08-14 22:59:07 -0400

    Added clear calculator function

--- main.js
+++ main.js
@@ -6,11 +6,16 @@ const currentDisplayNumber = document.querySelector("currentNumber");
 const previousDisplayNumber = document.querySelector("previousNumber");
 
 const equal = document.querySelector(".equal");
-equal.addEventListener("click", calculate);
+equal.addEventListener("click", () => {
+    if(currentNum != "" && previousNum != ""){
+        calculate();
+    }
+});
 
 const decimal = document.querySelector(".decimal");
 
 const clear = document.querySelector(".clear");
+clear.addEventListener("click", clearCalculator);
 
 const numberButtons = document.querySelector(".number");
 
@@ -79,4 +84,12 @@ function displayResult(){
         currentDisplayNumber.textContent.previousNum.slice(0,11) + "...";
     }
     currentDisplayNumber.textContent = previousNum;
+};
+
+function clearCalculator(){
+    currentNum = "";
+    previousNum = "";
+    operator = "";
+    currentDisplayNumber = "0";
+    previousDisplayNumber = "";
 };
